32e9fd173b1b4edc5f9d0701cf1f449628a06f30
razvigor <kamaradsasa@gmail.com>
2022-09-22 13:27:49 +0200

    bind, call, aplay

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 const root = document.getElementById('root');
 let output = '';
 
+//Bind, Call and Aplay methods
+
 let person = {
 	firstName: 'Sasa',
 	lastName: 'Trkulja',
@@ -8,53 +10,136 @@ let person = {
 	isAdult() {
 		return this.age >= 18;
 	},
+	printName() {
+		return this.firstName + ' ' + this.lastName;
+	},
 };
-output = person.isAdult();
-function registerUser(fName, lName) {
-	let person = {
-		firstName: fName,
-		lastName: lName,
-	};
-	return person;
-}
+//output = person.isAdult();
 
-output = JSON.stringify(registerUser('Sasa', 'Trkulja'));
+const printFullName = person.printName;
+//output = printFullName();
 
-const person1 = {
-	name: 'Trkulja Sasa',
-	age: 55,
+// Bind
+
+const counter = {
+	count: 0,
+	incrementCounter: function () {
+		console.log(this);
+		this.count++;
+		console.log(this.count);
+	},
 };
-const healthCare = {
-	height: 185,
-	weight: 73,
-};
+const bindedIncrenebtCounter = counter.incrementCounter.bind(counter);
+// document
+// 	.querySelector('.btn')
+// 	.addEventListener('click', counter.incrementCounter);
 
-const copyPerson1 = { ...person1 };
-const copyPerson2 = Object.assign({}, person1);
+document
+	.querySelector('.btn')
+	.addEventListener('click', bindedIncrenebtCounter);
 
-output = JSON.stringify(copyPerson2);
+//Bind with argument
 
-//const mergedObj = Object.assign(person1, healthCare); mutira person1
+// function greeting(lang) {
+// 	console.log(`${lang}: I am ${this.name}`);
+// }
+// const sasa = {
+// 	name: 'Sasa',
+// };
+// const petar = {
+// 	name: 'Petar',
+// };
+// const greetingSasa = greeting.bind(sasa, 'sr');
+// greetingSasa();
+// const greetingPetar = greeting.bind(petar, 'ba');
+// greetingPetar();
+
+// Call
+
+// function greeting() {
+// 	console.log(`Hi, I am ${this.name} and I am ${this.age} years old`);
+// }
+// const sasa = {
+// 	name: 'Sasa',
+// 	age: 55,
+// };
+// const petar = {
+// 	name: 'Petar',
+// 	age: 35,
+// };
+
+// greeting.call(sasa);
+
+// greeting.call(petar);
+
+// Call with argument
+
+//Aplay
+
+// function greet(greeting, lang) {
+// 	console.log(lang);
+// 	console.log(`${greeting}, I am ${this.name} and I am ${this.age} years old`);
+// }
+// const sasa = {
+// 	name: 'Sasa',
+// 	age: 55,
+// };
+// const petar = {
+// 	name: 'Petar',
+// 	age: 35,
+// };
+
+// greet.apply(sasa, ['Hi', 'sr']);
+
+// greet.apply(petar, ['Hola', 'ba']);
+
+//Object merge
+
+// function registerUser(fName, lName) {
+// 	let person = {
+// 		firstName: fName,
+// 		lastName: lName,
+// 	};
+// 	return person;
+// }
+
+// output = JSON.stringify(registerUser('Sasa', 'Trkulja'));
+
+// const person1 = {
+// 	name: 'Trkulja Sasa',
+// 	age: 55,
+// };
+// const healthCare = {
+// 	height: 185,
+// 	weight: 73,
+// };
+
+// const copyPerson1 = { ...person1 };
+// const copyPerson2 = Object.assign({}, person1);
+
+// output = JSON.stringify(copyPerson2);
+
+//const mergedObj = Object.assign(person1, healthCare); //mutira person1
 //const mergedObj = Object.assign({}, person1, healthCare);
 
-const mergedObj = function (person, healtCare) {
-	return Object.assign({}, person, healtCare);
-};
-output = JSON.stringify(mergedObj(person1, healthCare));
+// const mergedObj = function (person, healtCare) {
+// 	return Object.assign({}, person, healtCare);
+// };
+// output = JSON.stringify(mergedObj(person1, healthCare));
 
 //Constructor functions
-function Person(firstName, lastName, age) {
-	this.firstName = firstName;
-	this.lastName = lastName;
-	this.age = age;
-	this.isAdult = function () {
-		return this.age >= 18;
-	};
-}
+// function Person(firstName, lastName, age) {
+// 	this.firstName = firstName;
+// 	this.lastName = lastName;
+// 	this.age = age;
+// 	this.isAdult = function () {
+// 		return this.age >= 18;
+// 	};
+// }
 
-const sasa = new Person('Sasa', 'Trkulja', 55);
+// const sasa = new Person('Sasa', 'Trkulja', 55);
 
-output = JSON.stringify(sasa);
+// output = JSON.stringify(sasa);
 
 //output = sasa.isAdult();
 
@@ -65,160 +150,161 @@ output = JSON.stringify(sasa);
 //Object.defineProperty(sasa, 'firstName', { enumerable: true });
 //Object.defineProperty(sasa, 'firstName', { writable: true });
 
-output = JSON.stringify(Object.getOwnPropertyDescriptor(sasa, 'firstName'));
+// output = JSON.stringify(Object.getOwnPropertyDescriptor(sasa, 'firstName'));
 
-Object.defineProperty(sasa, 'fullName', {
-	get: function () {
-		return this.firstName + ' ' + this.lastName;
-	},
-	set: function (value) {
-		let nameParts = value.split(' ');
-		this.firstName = nameParts[0];
-		this.lastName = nameParts[1];
-	},
-});
+// Object.defineProperty(sasa, 'fullName', {
+// 	get: function () {
+// 		return this.firstName + ' ' + this.lastName;
+// 	},
+// 	set: function (value) {
+// 		let nameParts = value.split(' ');
+// 		this.firstName = nameParts[0];
+// 		this.lastName = nameParts[1];
+// 	},
+// });
 
-sasa.fullName = 'Petar Petrovic';
+// sasa.fullName = 'Petar Petrovic';
 
-output = sasa.fullName;
+// output = sasa.fullName;
 
-let myFunc = function () {};
+// let myFunc = function () {};
 
-Person.prototype.job = 'Frontend developer';
+// Person.prototype.job = 'Frontend developer';
 
-output = JSON.stringify(myFunc.prototype);
+// output = JSON.stringify(myFunc.prototype);
 
-output = JSON.stringify(Person.prototype);
+// output = JSON.stringify(Person.prototype);
 
-output = JSON.stringify(sasa.__proto__);
+// output = JSON.stringify(sasa.__proto__);
 
-output = Person.prototype === sasa.__proto__;
+// output = Person.prototype === sasa.__proto__;
 
 //sasa.job = 'Backend developer';
 
-output = sasa.hasOwnProperty('job');
+// output = sasa.hasOwnProperty('job');
 
-function Human(firstName, lastName, age) {
-	this.firstName = firstName;
-	this.lastName = lastName;
-	this.age = age;
+// function Human(firstName, lastName, age) {
+// 	this.firstName = firstName;
+// 	this.lastName = lastName;
+// 	this.age = age;
 
-	Object.defineProperty(this, 'fullName', {
-		get: function () {
-			return this.firstName + ' ' + this.lastName;
-		},
-		set: function (value) {
-			let nameParts = value.split(' ');
-			this.firstName = nameParts[0];
-			this.lastName = nameParts[1];
-		},
-		enumerable: true,
-	});
-}
+// 	Object.defineProperty(this, 'fullName', {
+// 		get: function () {
+// 			return this.firstName + ' ' + this.lastName;
+// 		},
+// 		set: function (value) {
+// 			let nameParts = value.split(' ');
+// 			this.firstName = nameParts[0];
+// 			this.lastName = nameParts[1];
+// 		},
+// 		enumerable: true,
+// 	});
+// }
 
-function Student(firstName, lastName, age) {
-	Human.call(this, firstName, lastName, age);
-	this._enroledCourses = [];
-	this.enroll = function (courseName) {
-		this._enroledCourses.push(courseName);
-	};
-	this.printCourses = function () {
-		return (
-			this.fullName +
-			"'s enrolled courses are: " +
-			this._enroledCourses.join(', ')
-		);
-	};
-}
-output = Student.prototype.constructor;
+// function Student(firstName, lastName, age) {
+// 	Human.call(this, firstName, lastName, age);
+// 	this._enroledCourses = [];
+// 	this.enroll = function (courseName) {
+// 		this._enroledCourses.push(courseName);
+// 	};
+// 	this.printCourses = function () {
+// 		return (
+// 			this.fullName +
+// 			"'s enrolled courses are: " +
+// 			this._enroledCourses.join(', ')
+// 		);
+// 	};
+// }
+// output = Student.prototype.constructor;
 
-Student.prototype = Object.create(Human.prototype);
+// Student.prototype = Object.create(Human.prototype);
 
-output = Student.prototype.constructor;
+// output = Student.prototype.constructor;
 
-Student.prototype.constructor = Student;
+// Student.prototype.constructor = Student;
 
-output = Student.prototype.constructor;
+// output = Student.prototype.constructor;
 
-const petar = new Student('Petar', 'Petrovic', 22);
+// const petar = new Student('Petar', 'Petrovic', 22);
 
-petar.enroll('HTML');
-petar.enroll('CSS');
-petar.enroll('javaScript');
+// petar.enroll('HTML');
+// petar.enroll('CSS');
+// petar.enroll('javaScript');
 
-output = JSON.stringify(petar);
-output = petar.printCourses();
+// output = JSON.stringify(petar);
+// output = petar.printCourses();
 
-console.log(petar.__proto__);
-console.log(petar.__proto__.__proto__);
+// console.log(petar.__proto__);
+// console.log(petar.__proto__.__proto__);
 
 // Classes
 
-class ClassHuman {
-	constructor(firstName, lastName, age) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.age = age;
-		this.date = new Date();
-	}
-	get fullName() {
-		return this.firstName + ' ' + this.lastName;
-	}
-	set fullName(fullName) {
-		let nameParts = fullName.split(' ');
-		this.firstName = nameParts[0];
-		this.lastName = nameParts[1];
-	}
-	get costumizeDate() {
-		return this.date.toISOString().slice(0, 10).replaceAll('-', ' : ');
-	}
-	isAdult() {
-		return this.age >= 18;
-	}
-}
+// class ClassHuman {
+// 	constructor(firstName, lastName, age) {
+// 		this.firstName = firstName;
+// 		this.lastName = lastName;
+// 		this.age = age;
+// 		this.date = new Date();
+// 	}
+// 	get fullName() {
+// 		return this.firstName + ' ' + this.lastName;
+// 	}
+// 	set fullName(fullName) {
+// 		let nameParts = fullName.split(' ');
+// 		this.firstName = nameParts[0];
+// 		this.lastName = nameParts[1];
+// 	}
+// 	get costumizeDate() {
+// 		return this.date.toISOString().slice(0, 10).replaceAll('-', ' : ');
+// 	}
+// 	isAdult() {
+// 		return this.age >= 18;
+// 	}
+// }
 
 //console.log(typeof ClassHuman);
 
-Object.defineProperty(ClassHuman.prototype, 'fullName', {
-	enumerable: true,
-});
+// Object.defineProperty(ClassHuman.prototype, 'fullName', {
+// 	enumerable: true,
+// });
 
-const me = new ClassHuman('Sasa', 'Trkulja', 55);
+// const me = new ClassHuman('Sasa', 'Trkulja', 55);
 
 //me.fullName = 'Petar Petrovic';
 
-output = JSON.stringify(me);
+//output = JSON.stringify(me);
 // output = me.fullName;
 // output = me.date;
 // output = me.costumizeDate;
 // output = me.isAdult();
 
 //output = JSON.stringify(Object.getOwnPropertyDescriptor(me, 'fullName'));
-class ClassStudent extends ClassHuman {
-	constructor(firstName, lastName, age) {
-		super(firstName, lastName, age);
-		this._enroledCourses = [];
-	}
-	static fromClassHuman(person) {
-		return new Student(person.firstName, person.lastName, person.age);
-	}
-	enroll(courseName) {
-		this._enroledCourses.push(courseName);
-	}
-	printCourses() {
-		return (
-			this.fullName +
-			"'s enrolled courses are: " +
-			this._enroledCourses.join(', ')
-		);
-	}
-}
+// class ClassStudent extends ClassHuman {
+// 	constructor(firstName, lastName, age) {
+// 		super(firstName, lastName, age);
+// 		this._enroledCourses = [];
+// 	}
+// 	static fromClassHuman(person) {
+// 		return new Student(person.firstName, person.lastName, person.age);
+// 	}
+// 	enroll(courseName) {
+// 		this._enroledCourses.push(courseName);
+// 	}
+// 	printCourses() {
+// 		return (
+// 			this.fullName +
+// 			"'s enrolled courses are: " +
+// 			this._enroledCourses.join(', ')
+// 		);
+// 	}
+// }
 
 //const meStudent = new ClassStudent('Sasa', 'Trkulja', 55);
-const meStudent = ClassStudent.fromClassHuman(me);
-meStudent.enroll('javaScript');
-meStudent.enroll('HTML');
-meStudent.enroll('CSS');
+// const meStudent = ClassStudent.fromClassHuman(me);
+// meStudent.enroll('javaScript');
+// meStudent.enroll('HTML');
+// meStudent.enroll('CSS');
 
-output = meStudent.printCourses();
+// output = meStudent.printCourses();
+
 root.innerHTML = output;
